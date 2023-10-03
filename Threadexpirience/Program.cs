f016a75325beaa29741abd5433775234133e2969
Timur <User@LAPTOP-Q3NBDU5U>
2023-10-03 23:33:02 +0400

    Add project files.

---
+++ Threadexpirience/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Thread myThread = new Thread(Print);
+        // запускаем поток myThread
+        myThread.Start();
+
+        // действия, выполняемые в главном потоке
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"Главный поток: + {i}");
+            Thread.Sleep(300);
+        }
+
+        // действия, выполняемые во втором потокке
+        void Print()
+        {
+            string a = "agda";
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Второй поток: {i} {a}");
+                Thread.Sleep(400);
+            }
+        }
+    }
+}
